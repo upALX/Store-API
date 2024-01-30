d30d8fb232280d912c844207e12b1a011a5a243e
Alexandre <alexandreaddres@gmail.com>
2024-01-30 01:53:09 -0300

    create category sql to get and create categories

--- internal/database/category_db.go
+++ internal/database/category_db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+
 	"github.com/upalx/Store-API/internal/entity"
 )
 
@@ -13,10 +14,10 @@ func NewCategoryDB(db *sql.DB) *CategoryDB {
 	return &CategoryDB{db: db}
 }
 
-func (cd *CategoryDB) GetCategories() ([]*entity.Category, error){
-	rows, err:= cd.db.Query("SELECT id, name FROM categories")
+func (cd *CategoryDB) GetCategories() ([]*entity.Category, error) {
+	rows, err := cd.db.Query("SELECT id, name FROM categories")
 
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
@@ -24,13 +25,31 @@ func (cd *CategoryDB) GetCategories() ([]*entity.Category, error){
 
 	var categories []*entity.Category
 
-	for rows.Next(){
+	for rows.Next() {
 		var category entity.Category
 
-		if err := rows.Scan(&category.ID, &category.Name); err != nil{
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
 			return nil, err
 		}
 		categories = append(categories, &category)
 	}
 	return categories, nil
+}
+
+func (cd *CategoryDB) GetCategory(id string) (*entity.Category, error) {
+	var category entity.Category
+	err := cd.db.QueryRow("SELECT id, name FROM categories WHERE id = ?", id).Scan(&category.ID, &category.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (cd *CategoryDB) CreateCategory(category *entity.Category) (string, error) {
+	_, err := cd.db.Exec("INSERT INTO categories (id, name) VALUES (?,?)", category.ID, category.Name)
+
+	if err != nil {
+		return "", err
+	}
+	return category.ID, nil
 }
